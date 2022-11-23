8e1242b1e9aef94c3b900f6f4af32625821ad43f
Ruben Kraus <s80350@htw-dresden.de>
2022-11-23 16:42:43 +0100

    changed to return only two values

--- Katalog.php
+++ Katalog.php
@@ -4,16 +4,15 @@
     or die("Keine Verbindung zur Datenbank moeglich: ");
     echo "<span> Verbindung erfolgreich. </span>";
 
-    $query = "Select Produkttitel, Autorenname FROM buecher;";
+    $query = "Select * FROM buecher;";
     $result = $db_con-> query($query) or die("Anfrage fehlgeschlagen");
     if($result -> num_rows > 0) {
         echo "<table>\n";
         while($line = $result->fetch_assoc()) {
-            echo "\t<tr>\n";
-            foreach($line as $value) {
-                echo "\t\t<td>$value</td>\n";
-            }
-            echo "\t</tr>\n";
+            echo "\t<a href='katalog.php?detailId=" . $line['Id']> "<tr>\n";
+            echo "\t\t<td>" . $line['Produkttitel'] . "</td>\n";
+            echo "\t\t<td>" . $line['Autorenname'] . "</td>\n";
+            echo "\t</tr></a>\n";
         }
         echo "</table>\n";
     }
