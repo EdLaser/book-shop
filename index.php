6b43979c001338b0eb8b7d42beaf146952622475
Ruben Kraus <s80350@htw-dresden.de>
2022-11-24 10:21:41 +0100

    used new components

--- index.php
+++ index.php
@@ -1,104 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bookshop</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-        crossorigin="anonymous"></script>
-</head>
+<?php include './components/head.php' ?>
 
 <body>
-    <header class="d-flex p-3 bg-dark text-white justify-content-center">
-        <ul class="nav nav-pills">
-            <li>
-                <h1>
-                    Buchgeschäfft der Gruppe MR
-                </h1>
-            </li>
-        </ul>
-    </header>
-    <div class="d-flex flex-row">
-        <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark fixed-right" style="width: 22.5%;">
-            <span class="fs-4">
-                Sidebar
-            </span>
-            <hr>
-            <ul class="nav nav-pills flex-column mb-auto">
-                <li class="nav-item">
-                    <a href="#" class="nav-link text-white">
-                        EwA Gruppe G04
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <div class="accordion" id="bookShopNavList">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="bookShopHeader">
-                                <button class="accordion-button collapsed text-white bg-dark" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#linkList" aria-expanded="false" aria-controls="linkList">
-                                    Buchshop
-                                </button>
-                            </h2>
-                        </div>
-                        <div id="linkList" class="accordion-collapse collapse" aria-labelledby="bookShopHeader"
-                            data-bs-parent="#bookShopNavList">
-                            <div class="accordion-body">
-                                <ul class="list-group">
-                                    <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                                        First item
-                                    </a>
-                                    <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                                        Second item
-                                    </a>
-                                    <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                                        Third item
-                                    </a>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link text-white">
-                        Verwaltung
-                    </a>
-                </li>
-            </ul>
-            <hr>
+    <?php include './components/header.php' ?>
+    <main class="bg-dark vh-100">
+        <div class="d-flex flex-row">
+            <?php include './components/sidebar.php' ?>
+            <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark fixed-right" style="width: 55%;">
+                test
+            </div>
+            <?php include './components/aside.php' ?>
         </div>
-        <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark fixed-right" style="width: 55%;">
-            test
-        </div>
-        <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark fixed-right" style="width: 22.5%;">
-            <span class="fs-4">
-                Aside
-            </span>
-            <hr>
-            Test content on the side
-            <hr>
-        </div>
-    </div>
-    <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item px-2 text-muted">
-                s80350
-            </li>
-            <li class="nav-item px-2 text-muted">
-                s80485
-            </li>
-            <li class="nav-item px-2 text-muted">
-                <a class="text-muted" href="file:///Users/ruben/dev/ewa/book-shop/contact.html">Kontakt</a>
-            </li>
-        </ul>
-        <p class="text-center text-muted">
-            © HTW-Dresden
-        </p>
-    </footer>
+    </main>
+    <?php include './components/footer.php' ?>
 </body>
 
 </html>
