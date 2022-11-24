afbac61c856a8707d241761afe2b6233202ad219
Ruben Kraus <s80350@htw-dresden.de>
2022-11-24 08:08:55 +0100

    test of detail view

--- detail.php
+++ detail.php
@@ -1,6 +1,21 @@
 <?php
-    echo htmlspecialchars($_GET['detailId']);
+    echo htmlspecialchars($_GET['detailID']);
+    $id_of_book = $_GET['detailID'];
+    $db_con = new mysqli("localhost", "root", "ed67ford", "g04")
+    or die("Keine Verbindung zur Datenbank moeglich: ");
+    echo "<span> Verbindung erfolgreich. </span>";
 
-    
+    $query = "Select * FROM buecher WHERE id =" . $id_of_book . ";";
+    $result = $db_con-> query($query) or die("Anfrage fehlgeschlagen");
 
+    if($result -> num_rows > 0) {
+        echo "<table>";
+        while($line = $result-> fetch_assoc()) {
+            echo "\t<tr>\n";
+            foreach($line as $col_val) {
+                echo "<td>$col_value</td>";
+            }
+            echo "\t</tr>\n";
+        }
+    }
 ?>
