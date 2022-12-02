7b41e637dc5898ec2991813341526bb9b3ac31cf
Ruben Kraus <s80350@htw-dresden.de>
2022-12-02 19:17:51 +0100

    query version

--- Katalog.php
+++ Katalog.php
@@ -9,7 +9,37 @@
         <div class="d-flex flex-row">
             <?php include './components/sidebar.php' ?>
             <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark fixed-right" style="width: 55%;">
+                <div class="navbar">
+                    <div class="container-fluid">
+                        <form action="katalog.php" class="d-flex" role="search" method="GET">
+                            <input class="form-control me-2" type="search" placeholder="Titel" aria-label="Titel" name="titel">
+                            <input class="form-control me-2" type="searchs" placeholder="Autor" aria-label="Autor" name="autor">
+                            <button class="btn btn-outline-primary" type="submit">Suchen</button>
+                        </form>
+                    </div>
+                </div>
+
                 <?php
+                function search()
+                {
+                    $search_query = "SELECT * FROM buecher WHERE ";
+                    // $_GET = array_filter($_GET);
+
+                    if (!empty($_GET['titel']) and !empty($_GET['autor'])) {
+                        // Both set
+                        $search_query .= "Produkttitel = " . $_GET['titel'] . "AND Autorenname = '" . $_GET['autor'] ."'";
+                    } elseif (empty($_GET['titel']) and !empty($_GET['autor'])) {
+                        // No Title
+                        $search_query .= "Autorenname = '" . $_GET['autor'] ."'";
+                    } elseif (empty($_GET['autor']) and !empty($_GET['titel'])) {
+                        // No Author but Title
+                        $search_query .= "Produkttitel = '" . $_GET['titel'] ."'";
+                    }
+
+                    $search_query .= ";";
+                    return $search_query;
+                }
+
                 function displayOutput($result)
                 {
                     if ($result->num_rows > 0) {
@@ -29,18 +59,18 @@
                         echo "</table>\n";
                     }
                 }
-                
                 // host, user, password, database
                 $db_con = new mysqli("localhost", "g04", "ed67ford", "g04")
                     or die("Keine Verbindung zur Datenbank moeglich: ");
 
-                $query = "Select * FROM buecher;";
+                (isset($_GET['autor']) or isset($_GET['titel'])) ? $query = search() : $query = "Select * FROM buecher;";
+                echo $query;
                 $result = $db_con->query($query) or die("Anfrage fehlgeschlagen");
-                
+
                 displayOutput($result);
-                
-                
-                
+
+
+
                 $db_con->close();
                 ?>
             </div>
