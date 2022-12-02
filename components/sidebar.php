5f4962387b897b2be0ce509b87c581f5f5d38565
Ruben Kraus <s80350@htw-dresden.de>
2022-12-02 18:05:15 +0100

    updated navbar

--- components/sidebar.php
+++ components/sidebar.php
@@ -10,30 +10,19 @@
             </a>
         </li>
         <li class="nav-item">
-            <div class="accordion" id="bookShopNavList">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="bookShopHeader">
-                        <button class="accordion-button collapsed text-white bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#linkList" aria-expanded="false" aria-controls="linkList">
-                            Buchshop
-                        </button>
-                    </h2>
-                </div>
-                <div id="linkList" class="accordion-collapse collapse" aria-labelledby="bookShopHeader" data-bs-parent="#bookShopNavList">
-                    <div class="accordion-body">
-                        <ul class="list-group">
-                            <a href="index.php" class="list-group-item list-group-item-action bg-dark text-white">
-                                Home
-                            </a>
-                            <a href="./katalog.php" class="list-group-item list-group-item-action bg-dark text-white">
-                                Katalog
-                            </a>
-                            <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                                Second
-                            </a>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            <a href="index.php" class="list-group-item list-group-item-action bg-dark text-white">
+                Home
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="" class="nav-link text-white">
+                Buchshop
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="./katalog.php" class="nav-link text-white">
+                Katalog
+            </a>
         </li>
         <li class="nav-item">
             <a href="" class="nav-link text-white">
