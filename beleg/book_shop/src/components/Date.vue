c4c7ea6ed2f32c24a7d9b82a8c1870099c8d6eb0
Ruben Kraus <s80350@htw-dresden.de>
2022-12-15 21:33:14 +0100

    fixed bug

--- beleg/book_shop/src/components/Date.vue
+++ beleg/book_shop/src/components/Date.vue
@@ -13,7 +13,7 @@ export default {
         setDate() {
             const date = new Date()
             let day = date.getDate();
-            let month = date.getMonth();
+            let month = date.getMonth() + 1;
             let year = date.getFullYear();
 
             this.date = `${day}.${month}.${year}`
