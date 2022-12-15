b393a82f5a123dd329c84e7f66f3aeca147482e1
Ruben Kraus <s80350@htw-dresden.de>
2022-12-15 17:00:12 +0100

    changed setup

--- beleg/book_shop/src/App.vue
+++ beleg/book_shop/src/App.vue
@@ -1,7 +1,20 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import BookShopView from './views/BookShopView.vue';
 </script>
 
 <template>
+    <header class="d-flex p-3 bg-dark text-white justify-content-center">
+        <ul class="nav nav-pills">
+            <li>
+                <h1>Buchgeschäfft der Gruppe G04</h1>
+            </li>
+        </ul>
+    </header>
+    <main class="bg-dark vh-100">
+        <div class="container">
+            <BookShopView />
+        </div>
+    </main>
 
 </template>
