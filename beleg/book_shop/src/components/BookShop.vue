0894f3a0ba1b7236a9195b4605408ab5b6b44019
Ruben Kraus <s80350@htw-dresden.de>
2022-12-15 18:51:19 +0100

    added bookgrid to bookshop

--- beleg/book_shop/src/components/BookShop.vue
+++ beleg/book_shop/src/components/BookShop.vue
@@ -1,11 +1,14 @@
 <script>
 import Date from './Date.vue';
 import Time from './Time.vue';
+import BookGrid from './BookGrid.vue';
 
 export default {
     components: {
-        Date, Time
-    }
+    Date,
+    Time,
+    BookGrid
+}
 }
 </script>
 
@@ -16,6 +19,11 @@ export default {
         </div>
         <div class="d-flex flex-column text-bg-dark">
             <Time />
+        </div>
+    </div>
+    <div class="d-flex flex-row">
+        <div class="d-flex flex-column">
+            <BookGrid />
         </div>
     </div>
 </template>
