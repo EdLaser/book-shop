1728d8caf6ed9c5b671a0d96aafe6325ab19a1fc
Ruben Kraus <s80350@htw-dresden.de>
2022-12-15 20:33:25 +0100

    php to generate json as result

---
+++ beleg/book_shop/fetch_books.php
@@ -0,0 +1,21 @@
+<?php
+
+$db_con = new mysqli("localhost", "g04", "ed67ford", "g04")
+    or die("Keine Verbindung zur Datenbank moeglich: ");
+
+$result = $db_con->query("SELECT * FROM buecher;") or die("Anfrage fehlgeschlagen");
+
+if ($result->num_rows > 0) {
+    $result_array = [];
+    while ($line = $result->fetch_assoc()) {
+        array_push($result_array, $line);
+    }
+    $db_con->close();
+
+    $json = json_encode(($result_array));
+    $temp_file = fopen("books.json", "w");
+
+    fwrite($temp_file, $json);
+    fclose($temp_file);
+}
+?>
