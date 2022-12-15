696a6c75d34416a3534e2d0e088218e2d5eabc76
Ruben Kraus <s80350@htw-dresden.de>
2022-12-15 20:50:58 +0100

    rtied but charset it not ritgh

--- beleg/book_shop/fetch_books.php
+++ beleg/book_shop/fetch_books.php
@@ -4,7 +4,7 @@ $db_con = new mysqli("localhost", "g04", "ed67ford", "g04")
     or die("Keine Verbindung zur Datenbank moeglich: ");
 
 $db_con->set_charset("utf8");
-
+$db_con->query("SET NAMES 'utf8' COLLATE 'utf8_unicode_ci' ");
 $result = $db_con->query("SELECT * FROM buecher;") or die("Anfrage fehlgeschlagen");
 
 if ($result->num_rows > 0) {
