a2d4895de5b138ba01950016108cbb18d12c946a
Ruben Kraus <s80350@htw-dresden.de>
2022-12-15 17:44:29 +0100

    setThe Date

--- beleg/book_shop/src/components/Date.vue
+++ beleg/book_shop/src/components/Date.vue
@@ -1,7 +1,26 @@
-<script setup>
-
-</script>
-
 <template>
-    <h2 id="date">Test Date</h2>
+    <h2 id="date">Date: {{ date }}</h2>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            date: ""
+        }
+    },
+    methods: {
+        setDate() {
+            const date = new Date()
+            let day = date.getDate();
+            let month = date.getMonth();
+            let year = date.getFullYear();
+
+            this.date = `${day}.${month}.${year}`
+        }
+    },
+    mounted() {
+        this.setDate();
+    }
+}
+</script>
