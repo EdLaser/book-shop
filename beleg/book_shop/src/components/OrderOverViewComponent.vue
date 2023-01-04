193ebae5c1fc5241490d182bcb5f146682dc0edd
Ruben Kraus <s80350@htw-dresden.de>
2023-01-04 17:52:47 +0100

    added input for count of books

--- beleg/book_shop/src/components/OrderOverViewComponent.vue
+++ beleg/book_shop/src/components/OrderOverViewComponent.vue
@@ -1,3 +1,44 @@
+<template>
+    <span class="fs-4">
+        Bestellübersicht
+    </span>
+    <hr>
+    <table class="table table-dark table-hover">
+        <thead>
+            <th scope="col">
+            <td>#</td>
+            </th>
+            <th scope="col">
+            <td>Titel</td>
+            </th>
+            <th scope="col">
+            <td>Anzahl</td>
+            </th>
+        </thead>
+        <tbody v-if="orderItems > 0">
+            <tr v-for="(orderItem, index) in order">
+                <td>
+                    {{ index + 1 }}
+                </td>
+                <td>
+                    {{ orderItem.title }}
+                </td>
+                <td>
+                    <input type="number" v-model="orderItem.count" @change="sumPrice()">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="row">
+        <div class="col"><span>Anzahl Bücher: {{ this.bookAmount }}</span></div>
+        <div class="col"><span>Preis: {{ this.price }} €</span></div>
+    </div>
+    <hr>
+    <form action="/checkout">
+        <button type="submit" class="btn btn-success">Bestellung abschließen</button>
+    </form>
+    <hr>
+</template>
 <script>
 import { store } from './store';
 
@@ -28,6 +69,14 @@ export default {
                 price += item.price * item.count, 2;
             }
             this.price = price.toFixed(2)
+        },
+        decrease(orderItem) {
+            let item = store.order.find(item => item.title === orderItem.title);
+            item.count > 1 ? item.count-- : store.order = this.removeByTitle(store.order, orderItem.title);
+            store.bookAmount--;
+        },
+        removeByTitle(list, title) {
+            return list.filter(item => item.title !== title)
         }
     },
     watch: {
@@ -37,45 +86,3 @@ export default {
     }
 }
 </script>
-
-<template>
-    <span class="fs-4">
-        Bestellübersicht
-    </span>
-    <hr>
-    <table class="table table-dark table-hover">
-        <thead>
-            <th scope="col">
-            <td>#</td>
-            </th>
-            <th scope="col">
-            <td>Titel</td>
-            </th>
-            <th scope="col">
-            <td>Anzahl</td>
-            </th>
-        </thead>
-        <tbody v-if="orderItems > 0">
-            <tr v-for="(orderItem, index) in order">
-                <td>
-                    {{ index + 1 }}
-                </td>
-                <td>
-                    {{ orderItem.title }}
-                </td>
-                <td>
-                    {{ orderItem.count }}
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <div class="row">
-        <div class="col"><span>Anzahl Bücher: {{ this.bookAmount }}</span></div>
-        <div class="col"><span>Preis: {{ this.price }} €</span></div>
-    </div>
-    <hr>
-    <form action="/checkout">
-        <button type="submit" class="btn btn-success">Bestellung abschließen</button>
-    </form>
-    <hr>
-</template>
