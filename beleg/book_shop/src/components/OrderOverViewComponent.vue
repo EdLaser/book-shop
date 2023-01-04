fcf9698c230bcda46e3c1f78c178a761cb9a4455
Ruben Kraus <s80350@htw-dresden.de>
2023-01-04 18:19:32 +0100

    changed methods and added delete button for elements

--- beleg/book_shop/src/components/OrderOverViewComponent.vue
+++ beleg/book_shop/src/components/OrderOverViewComponent.vue
@@ -5,15 +5,12 @@
     <hr>
     <table class="table table-dark table-hover">
         <thead>
-            <th scope="col">
-            <td>#</td>
-            </th>
-            <th scope="col">
-            <td>Titel</td>
-            </th>
-            <th scope="col">
-            <td>Anzahl</td>
-            </th>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titel</th>
+                <th scope="col">Anzahl</th>
+                <th scope="col">Preis</th>
+            </tr>
         </thead>
         <tbody v-if="orderItems > 0">
             <tr v-for="(orderItem, index) in order">
@@ -24,7 +21,20 @@
                     {{ orderItem.title }}
                 </td>
                 <td>
-                    <input type="number" v-model="orderItem.count" @change="sumPrice()">
+                    <input class="form-control" type="number" v-model="orderItem.count" @change="sumPrice()">
+                </td>
+                <td>
+                    {{ orderItem.price }} €
+                </td>
+                <td>
+                    <button type="button" class="btn btn-secondary" @click="removeByTitle(orderItem.title)"><svg
+                            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-trash" viewBox="0 0 16 16">
+                            <path
+                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                            <path fill-rule="evenodd"
+                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                        </svg></button>
                 </td>
             </tr>
         </tbody>
@@ -66,23 +76,22 @@ export default {
         sumPrice() {
             let price = 0;
             for (let item of this.order) {
-                price += item.price * item.count, 2;
+                item.count < 1 ? store.order = this.removeByTitle(item.title) : null;
+                price += item.price * item.count;
             }
             this.price = price.toFixed(2)
         },
-        decrease(orderItem) {
-            let item = store.order.find(item => item.title === orderItem.title);
-            item.count > 1 ? item.count-- : store.order = this.removeByTitle(store.order, orderItem.title);
-            store.bookAmount--;
-        },
-        removeByTitle(list, title) {
-            return list.filter(item => item.title !== title)
+        removeByTitle(title) {
+            console.log(title)
+            console.log(store.order)
+            store.order = store.order.filter(item => item.title !== title)
         }
     },
     watch: {
         bookAmount() {
             this.sumPrice();
         },
+        order() { }
     }
 }
 </script>
