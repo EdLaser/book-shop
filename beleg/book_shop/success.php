c0e302d6ca447a02830f75bf18fd6c62d5c1e69b
Ruben Kraus <s80350@htw-dresden.de>
2023-01-05 21:49:03 +0100

    success and cancel for stripe

---
+++ beleg/book_shop/success.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dankeschön!</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="d-flex p-3 bg-dark text-white justify-content-center">
+        <ul class="nav nav-pills">
+            <li>
+                <h1>Buchgeschäfft der Gruppe G04</h1>
+            </li>
+        </ul>
+    </header>
+    <main class="bg-dark vh-100 text-white">
+        <div class="container">
+            <div class="d-flex flex-row justify-content-center">
+                <div class="d-flex flex-column">
+                    <h1>Bezahlung erfolgreich</h1>
+                    <hr>
+                    <p>
+                        <?php
+                        echo "Danke für ihren Einkauf. Für Rückfragen, hier ihre Stripe-SessionID: " . $_GET['session_id'];
+                        ?>
+                    </p>
+                    <hr>
+                    <a href="/">Zurück zum Shop</a>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</html>
