b3082c2a8fa9e2602825942ea8aaab6c18493171
Ruben Kraus <s80350@htw-dresden.de>
2023-01-05 16:40:25 +0100

    checkout with multiple params

--- beleg/book_shop/src/helpers/create-checkout.php
+++ beleg/book_shop/src/helpers/create-checkout.php
@@ -1,17 +1,31 @@
 <?php
 
+
+$BOOK_MAPPING = [
+    'Das Schweigen der Manus' => 'price_1MMrhSKHP5yCVcwphIzLFZHt', 
+    'PHP für Dumme' => 'price_1MMuIlKHP5yCVcwpTr3uP2qV', 
+    'Docker ist auch nur ein Wal' => 'price_1MMuJJKHP5yCVcwpYEDJQ0DR'
+];
+
 require '../helpers/stripe-php-10.3.0/init.php';
 // This is your test secret API key.
 \Stripe\Stripe::setApiKey('sk_test_51MJx9SKHP5yCVcwp7iFbuCxnTb8cuyDxV7fCEeOC1eEb4ApA4D68ARdxevWIps2jKhtQ0lDpk38D8ZMWMnUbQDyc00zfn8DSjt');
 
-header('Content-Type: application/json');
-
 $YOUR_DOMAIN = 'http://localhost:5137/';
-$js_key = 'sk_test_51MJx9SKHP5yCVcwp7iFbuCxnTb8cuyDxV7fCEeOC1eEb4ApA4D68ARdxevWIps2jKhtQ0lDpk38D8ZMWMnUbQDyc00zfn8DSjt';
+
+$data = file_get_contents('php://input');
+$order = [];
+$json_data = json_decode($data, true);
+
+foreach($json_data as $val) {
+    if(array_key_exists($val['title'], $BOOK_MAPPING)) {
+        array_push($order, ['price' => $BOOK_MAPPING[$val['title']], 'quantity' => $val['amount']]);
+    }
+}
 
 $checkout_session = \Stripe\Checkout\Session::create([
     'line_items' => [
-        ['price' => 'price_1MMrhSKHP5yCVcwphIzLFZHt', 'quantity' => 10]
+        [$order]
     ],
     'mode' => 'payment',
     'success_url' => $YOUR_DOMAIN . '/success',
