4f9a28d9a531ba1ee27518538fc66fa98678b8f5
Ruben Kraus <s80350@htw-dresden.de>
2023-01-05 08:05:01 +0100

    moved fule

--- beleg/book_shop/src/components/create-checkout.php
+++ beleg/book_shop/src/components/create-checkout.php
@@ -1,24 +1,23 @@
 <?php
 
-require 'vendor/autoload.php';
+require '../helpers/stripe-php-10.3.0/init.php';
 // This is your test secret API key.
 \Stripe\Stripe::setApiKey('sk_test_51MJx9SKHP5yCVcwp7iFbuCxnTb8cuyDxV7fCEeOC1eEb4ApA4D68ARdxevWIps2jKhtQ0lDpk38D8ZMWMnUbQDyc00zfn8DSjt');
 
 header('Content-Type: application/json');
 
-$YOUR_DOMAIN = 'http://localhost:4242/public';
+$YOUR_DOMAIN = 'http://localhost:5137/';
+
+echo "$_GET";
 
 $checkout_session = \Stripe\Checkout\Session::create([
-  'line_items' => [[
-    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-    'price' => '{{PRICE_ID}}',
-    'quantity' => 1,
-  ]],
-  'mode' => 'payment',
-  'success_url' => $YOUR_DOMAIN . '/success.html',
-  'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+    'line_items' => [
+        $items
+    ],
+    'mode' => 'payment',
+    'success_url' => $YOUR_DOMAIN . '/success',
+    'cancel_url' => $YOUR_DOMAIN . '/cancel',
 ]);
 
 header("HTTP/1.1 303 See Other");
 header("Location: " . $checkout_session->url);
-?>
