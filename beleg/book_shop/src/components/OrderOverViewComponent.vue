3b115e3cca2123c03bfbe8178a57f29726ae491b
Ruben Kraus <s80350@htw-dresden.de>
2023-01-05 22:04:56 +0100

    fixed remove by title on delete button

--- beleg/book_shop/src/components/OrderOverViewComponent.vue
+++ beleg/book_shop/src/components/OrderOverViewComponent.vue
@@ -35,14 +35,15 @@
                         {{ orderItem.price }} €
                     </td>
                     <td>
-                        <button type="button" class="btn btn-secondary" @click="removeByTitle(orderItem.title)"><svg
-                                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-trash" viewBox="0 0 16 16">
+                        <button type="button" class="btn btn-secondary"
+                            @click="removeFromOrder(orderItem.title)"><svg xmlns="http://www.w3.org/2000/svg"
+                                width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                 <path
                                     d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                 <path fill-rule="evenodd"
                                     d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                            </svg></button>
+                            </svg>
+                        </button>
                     </td>
                 </tr>
             </tbody>
@@ -82,8 +83,7 @@ export default {
             let price = 0;
             if (this.orderLength > 0) {
                 for (let item of store.order) {
-                    item.count < 1 ? 0 : price += item.price * item.count;;
-                    price += item.price * item.count;
+                    item.count < 1 ? 0 : price += item.price * item.count;
                 }
                 return price.toFixed(2)
             }
@@ -103,18 +103,17 @@ export default {
             let item = store.order.find(item => item.title === title);
             if (item) {
                 item.count += 1;
-                store.bookAmount += item.count
+                store.bookAmount += 1
             } else {
                 store.order.push({ "title": book.Produkttitel, "count": 1, "price": parseFloat(book.PreisNetto) });
                 store.bookAmount += 1
             }
         },
+        removeFromOrder(title) {
+            store.order = this.removeByTitle(title);
+        },
         removeByTitle(title) {
-            if (typeof store.order === 'undefined') {
-                store.order = new Array(0)
-            } else {
-                return store.order.filter(item => item.title !== title)
-            }
+            return store.order.filter(item => item.title !== title)
         },
         doCheckout() {
             var stripe = Stripe('pk_test_51MJx9SKHP5yCVcwpXnsPxNDUzEwIRyrd9h4NKwOXEbsKPG9mho3yI40mAT8A9e4h0AtB6u4RY9EnhfUBBCyY3CZ300Uy1xtSF8');
