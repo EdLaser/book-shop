b1611e233d8da1f6bfd0a1721f25c3a68513e38e
Ruben Kraus <s80350@htw-dresden.de>
2023-01-05 21:49:12 +0100

    updated to use cancel und succes php files

--- beleg/book_shop/src/helpers/create-checkout.php
+++ beleg/book_shop/src/helpers/create-checkout.php
@@ -11,7 +11,7 @@ require '../helpers/stripe-php-10.3.0/init.php';
 // This is your test secret API key.
 \Stripe\Stripe::setApiKey('sk_test_51MJx9SKHP5yCVcwp7iFbuCxnTb8cuyDxV7fCEeOC1eEb4ApA4D68ARdxevWIps2jKhtQ0lDpk38D8ZMWMnUbQDyc00zfn8DSjt');
 
-$YOUR_DOMAIN = 'http://localhost:5137/';
+$YOUR_DOMAIN = 'https://ivm108.informatik.htw-dresden.de/ewa/g04/beleg/';
 
 $data = file_get_contents('php://input');
 $order = [];
@@ -28,8 +28,8 @@ $checkout_session = \Stripe\Checkout\Session::create([
         [$order]
     ],
     'mode' => 'payment',
-    'success_url' => $YOUR_DOMAIN . '/success',
-    'cancel_url' => $YOUR_DOMAIN . '/cancel',
+    'success_url' => $YOUR_DOMAIN . 'success.php?session_id={CHECKOUT_SESSION_ID}',
+    'cancel_url' => $YOUR_DOMAIN . 'cancel.php?session_id={CHECKOUT_SESSION_ID}'
 ]);
 $checkout_session_id = $checkout_session->id;
 echo $checkout_session_id;
