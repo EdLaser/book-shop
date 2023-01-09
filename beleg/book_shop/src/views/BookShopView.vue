40969edfc14fc49546b31c6975def1b07a7ab939
Ruben Kraus <s80350@htw-dresden.de>
2023-01-09 12:25:58 +0100

    added back cotnainer

--- beleg/book_shop/src/views/BookShopView.vue
+++ beleg/book_shop/src/views/BookShopView.vue
@@ -16,22 +16,24 @@ export default {
 
 <template>
     <main class="bg-dark vh-100">
-        <div class="d-flex flex-row">
+        <div class="container bg-dark">
             <div class="d-flex flex-row">
-                <div class="d-flex flex-column text-bg-dark">
-                    <Date />
-                </div>
-                <div class="d-flex flex-column text-bg-dark">
-                    <Time />
+                <div class="d-flex flex-row">
+                    <div class="d-flex flex-column text-bg-dark">
+                        <Date />
+                    </div>
+                    <div class="d-flex flex-column text-bg-dark">
+                        <Time />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="d-flex flex-row justify-content-between">
-            <div class="d-flex flex-column">
-                <BookShop />
-            </div>
-            <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark fixed-right" style="width: 40%">
-                <OrderOverViewComponent />
+            <div class="d-flex flex-row justify-content-between">
+                <div class="d-flex flex-column">
+                    <BookShop />
+                </div>
+                <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark fixed-right" style="width: 40%">
+                    <OrderOverViewComponent />
+                </div>
             </div>
         </div>
     </main>
